8f1f81ab32babdd6499c7cf7be81a34af244eb43
Harris-Huntington <101505804+Harris-Huntington@users.noreply.github.com>
2023-09-05 21:36:33 +1000

    Lots of setup + Login, user create, home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GroupComponent } from './group/group.component';
+import { ChatComponent } from './chat/chat.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'login', component: LoginComponent}, {path: 'home', component: HomeComponent}, {path: 'group', component: GroupComponent}, {path: 'chat', component: ChatComponent}, {path: 'create-user', component: CreateUserComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GroupComponent } from './group/group.component';
+import { ChatComponent } from './chat/chat.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
+    GroupComponent,
+    ChatComponent,
+    CreateUserComponent,
   ],
   imports: [
     BrowserModule,
